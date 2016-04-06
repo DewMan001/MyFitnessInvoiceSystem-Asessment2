e3c802b46997dfb9e47ce6abeb0f36ecf34fdbbe
James Burton <deweymanster@gmail.com>
2016-04-06 15:35:28 +0800

    +Login screen created *Login screen has had the ever-loving bejeezus tested out of it >Coming up: Making the home form (that comes up after you sign in correctly) and linking the two again

--- MyFit-InvoiceSystem-Assess2/MyFit-InvoiceSystem-Assess2/Program.cs
+++ MyFit-InvoiceSystem-Assess2/MyFit-InvoiceSystem-Assess2/Program.cs
@@ -16,7 +16,7 @@ namespace MyFit_InvoiceSystem_Assess2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new form_Login());
         }
     }
 }
